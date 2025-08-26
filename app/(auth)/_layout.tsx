8e267e9ef4ad17bc26cf6fe58a6a89400cc3a693
Ahmad <aa2997168@gmail.com>
2025-08-26 16:24:58 +0300

    Implement full auth, tab navigation

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, KeyboardAvoidingView, ScrollView, Platform, Dimensions, ImageBackground, Image } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Slot } from 'expo-router'
+import { Redirect, Slot } from 'expo-router'
+import { images } from '@/constants'
+import CustomInput from '@/components/Custominput'
+import CustomButton from '@/components/CustomButton'
+import useAuthStore from '@/store/auth.store'
 
-export default function _layout() {
+export default function AuthLayout() {
+  const { isAuthenticated } = useAuthStore();
+  
+  if(isAuthenticated) return <Redirect href={"/"}/>
+
   return (
-    <SafeAreaView>
-      <Text>_ auth layout</Text>
-        <Slot/>
-    </SafeAreaView>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <ScrollView className='bg-white h-full' keyboardShouldPersistTaps="handled">
+            <View className='w-full relative' style={{height: Dimensions.get('screen').height / 2.25}}>
+                <ImageBackground source={images.loginGraphic} className='size-full rounded-b-lg' resizeMode='stretch'/>
+                <Image source={images.logo} className='size-48 self-center absolute -bottom-16 z-10'/>
+            </View>
+            <Slot />
+        </ScrollView>
+        
+    </KeyboardAvoidingView>
   )
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,9 +4,14 @@ import { FlatList, Pressable, Text, View, Image, TouchableOpacity } from "react-
 import { SafeAreaView } from "react-native-safe-area-context";
 import cn from "clsx";
 import CartButton from "@/components/CartButton";
+import useAuthStore from "@/store/auth.store";
 
 
 export default function Index() {
+
+  const { user } = useAuthStore();
+
+
   return (
     <SafeAreaView className="flex-1 bg-white">
       <FlatList
